3054125cb445fb69e24550bd1dc410e83ae7959e
leviatan89 <leviatan.89@gmail.com>
2020-02-09 14:03:49 +0000

    Blog Deleted and CV hardcoded

--- src/index.js
+++ src/index.js
@@ -9,23 +9,8 @@ const CONF = {
 	{
 		'name':'Victor',
 		'surname':'Sesma',
-		'summaryLines':'Passion Driven Dev',
-	},
-		'blogPosts':
-		{
-			0:{
-				'createdOn':'2018-03-27 01:03:00',
-				'createdBy':'Victor Sesma',
-				'postTitle':'First Post That Says You "Hello World"',
-				'postContent':'This is my first blog post. I will tell you cool stuff in here. For now it is just my hello world. I am online.'
-			}/*,
-			1:{
-				'createdOn':'2017-03-03 15:15:25',
-				'createdBy':'Victor Sesma',
-				'postTitle':'Second post!',
-				'postContent':'Super long content 2. Super long content 2. Super long content 2. Super long conten 2.'
-			}*/
-		}
+		'summaryLines':'Golang Developer Based in London',
+	}
 };
 
 ReactDOM.render(<App conf={CONF}/>, document.getElementById('root'));
